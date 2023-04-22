49d17ac48da26a49beb24aac81782db8d6f57fdf
Rahul Singh Chauhan <rahulsinghchauhanrj@gmail.com>
2023-04-22 20:17:29 +0530

    22-04-2023

--- src/Components/Goggles.js
+++ src/Components/Goggles.js
@@ -1,46 +1,8 @@
 import React from 'react'
-import Navbar from './Navbar';
-import Footer from './Footer';
-import ProductCard from './ProductCard';
-import Carousel from './Carousel';
 
 const Goggles = () => {
   return (
-    <>
-<Navbar/>
-<Carousel/>
-<ProductCard
-          imageSrc="https://images.unsplash.com/photo-1508643315917-6688bbbb4cb5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZXllJTIwd2VhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-          name="Product 1"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          price="$19.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1557775446-b5678ecaf086?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8ZXllJTIwd2VhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-          name="Product 2"
-          description="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          price="$29.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1625887022688-15bf883d94a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8ZXllJTIwd2VhcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-          name="Product 3"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          price="$39.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1631486676757-4f5ade178282?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGV5ZSUyMHdlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          name="Product 3"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          price="$39.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1631383591182-aa24205c089a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGV5ZSUyMHdlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          name="Product 3"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          price="$39.99"
-        />
-<Footer/>
-</>
+    <div>Goggles</div>
   )
 }
 
--- src/Components/Slipper.js
+++ src/Components/Slipper.js
@@ -1,46 +1,8 @@
 import React from 'react'
-import Navbar from './Navbar';
-import Footer from './Footer';
-import ProductCard from './ProductCard';
-import Carousel from './Carousel';
 
 const Slipper = () => {
   return (
-    <>
-<Navbar/>
-<Carousel/>
-<ProductCard
-          imageSrc="https://images.unsplash.com/photo-1603808033192-082d6919d3e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80"
-          name="Product 1"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          price="$19.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1588361861040-ac9b1018f6d5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9vdHdlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60.jpg"
-          name="Product 2"
-          description="Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          price="$29.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vdHdlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          name="Product 3"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          price="$39.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vdHdlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          name="Product 3"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          price="$39.99"
-        />
-        <ProductCard
-          imageSrc="https://images.unsplash.com/photo-1606107557195-0e29a4b5b4aa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Zm9vdHdlYXJ8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
-          name="Product 3"
-          description="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          price="$39.99"
-        />
-<Footer/>
-</>
+    <div>Slipper</div>
   )
 }
 
